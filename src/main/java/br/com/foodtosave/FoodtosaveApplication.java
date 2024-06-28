f14abe16b6afff944af1ec7c265b52b833bdc36e
Kelvin Adrian Vieira <kelvin_adrian3@hotmail.com>
2024-06-28 02:03:12 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/foodtosave/FoodtosaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.foodtosave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtosaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtosaveApplication.class, args);
+	}
+
+}
